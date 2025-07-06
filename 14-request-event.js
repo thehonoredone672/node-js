d15352df3103a0bbb392123915ba7dd18c444dad
thehonoredone672 <thehonoredone672@gmail.com>
2025-07-06 16:35:25 +0530

    added file 13,14,,15,16 and big-file-txt

--- 14-request-event.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-
-const  server = http.createServer();
-
-server.on('request',(req,res) => {
-    res.end('Hello World')
-})
-
-server.listen(5000)
--- 15-create-big-file.js
+++
@@ -1,4 +0,0 @@
-const { writeFileSync } = require('fs')
-for (let i=0; i<1000;i++){
-    writeFileSync('./content/big-file-txt',`hello ${i}\n`,{flag: 'a'})
-}
